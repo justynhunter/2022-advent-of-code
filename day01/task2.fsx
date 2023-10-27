66ede2a8c3add820ae4e5186223ec706dd522eee
Justyn Hunter <justynhunter@gmail.com>
2023-10-27 11:27:50 -0500

    day 1 task 2

---
+++ day01/task2.fsx
@@ -0,0 +1,35 @@
+open System
+open System.IO
+
+let input = File.ReadAllLines "./input.txt" |> List.ofSeq 
+
+let hasString line =
+  line <> ""
+
+let rec getByElf lines =
+  if lines |> List.isEmpty then
+    []
+  else
+    let linesForElf = 
+      lines
+      |> List.takeWhile hasString
+
+    let tail =
+      lines
+      |> List.skipWhile hasString
+      |> List.skip 1
+
+    linesForElf::(getByElf tail)
+
+let sum lines =
+  List.append lines [""]
+    |> getByElf 
+    |> List.map (fun l -> List.map int l) 
+    |> List.map List.sum
+    |> List.sortDescending
+    |> List.take 3
+    |> List.sum
+
+sum input
+    |> printfn "%d"
+
