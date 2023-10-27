6d1b2299b1fa558ccbe77572419ec171e413f5d2
Justyn Hunter <justynhunter@gmail.com>
2023-10-27 15:12:20 -0500

    day 3 task 1

---
+++ day03/task1.fsx
@@ -0,0 +1,31 @@
+open System
+open System.IO
+
+let input = File.ReadAllLines "./input.txt" |> Array.toSeq
+
+type Rucksack = { CompartmentOne: Set<char>; CompartmentTwo: Set<char> }
+
+let getRucksack line =
+  let compartments =
+    line
+    |> Seq.splitInto 2
+    |> Seq.map set
+    |> Seq.toList
+  
+  let [one; two] = compartments
+  { CompartmentOne = one; CompartmentTwo = two }
+
+let getDupe rucksack =
+  Set.intersect rucksack.CompartmentOne rucksack.CompartmentTwo
+  |> Seq.head
+
+let getScore item =
+  if Char.IsLower item then int item - 96
+  else int item - 38
+
+input
+|> Seq.map getRucksack
+|> Seq.map getDupe 
+|> Seq.map getScore
+|> Seq.sum
+|> printfn "%d"
